8f862a8c04e0e78d46792bd06358ed7414f94a8a
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-07-26 18:30:45 +0800

    Create sessionManager.js

---
+++ src/engine/network/sessionManager.js
@@ -0,0 +1,7 @@
+export function createSession() {
+  // generate unique sessionId for group play
+}
+
+export function joinSession(sessionId, playerName) {
+  // assign traits, city, track actions
+}
