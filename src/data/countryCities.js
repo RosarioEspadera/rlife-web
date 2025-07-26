0f7664304e5e7c53626c649fb83a59f079aa11b2
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-07-26 18:05:41 +0800

    Create countryCities.js

---
+++ src/data/countryCities.js
@@ -0,0 +1,28 @@
+export const countryData = [
+  {
+    code: "PH",
+    name: "Philippines",
+    flag: "🇵🇭",
+    region: "Asia",
+    language: "Filipino",
+    traits: ["Family-oriented", "Karaoke master"],
+    cities: [
+      { name: "Kidapawan", population: 160000, traits: ["Mountain vibes", "Earthquake-prone"], eventTags: ["SukaranFestival", "QuakeDrill"] },
+      { name: "Manila", population: 1800000, traits: ["Urban", "Loud", "Crowded"], eventTags: ["JeepneyStrike", "FloodPrep"] }
+    ],
+    eventPack: "ph_events.json"
+  },
+  {
+    code: "US",
+    name: "United States",
+    flag: "🇺🇸",
+    region: "North America",
+    language: "English",
+    traits: ["Individualistic", "Fast-paced"],
+    cities: [
+      { name: "New York", population: 8500000, traits: ["Cosmopolitan", "Ambitious"], eventTags: ["BroadwayAudition", "Snowstorm"] },
+      { name: "Austin", population: 1000000, traits: ["Startup scene", "Hot summers"], eventTags: ["TechConference", "BBQFestival"] }
+    ],
+    eventPack: "us_events.json"
+  }
+];
